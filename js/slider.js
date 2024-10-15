6c55fb3673280458079276b4ca608edc4651dafe
priyankraychura <priyankraychura@gmail.com>
2024-10-15 14:20:48 +0530

    slider chnage time increased

--- js/slider.js
+++ js/slider.js
@@ -7,14 +7,12 @@ const images = [
 let img = document.querySelector('.home-slider img');
 let num = 0;
 
-img.src = images[num]; // Initial image
-img.classList.add('active'); // Make the first image active
+img.src = images[num]; 
+img.classList.add('active'); 
 
 setInterval(() => {
-    // Fade out the current image
     img.classList.remove('active');
 
-    // Wait for the fade-out effect to complete (1 second)
     setTimeout(() => {
         if (num < images.length - 1) {
             num++;
@@ -22,10 +20,7 @@ setInterval(() => {
             num = 0;
         }
 
-        // Change the image source
         img.src = images[num];
-
-        // Fade in the new image
         img.classList.add('active');
-    }, 1500); // This delay matches the transition duration in the CSS
-}, 5000); // Change image every 5 seconds
+    }, 1500); 
+}, 7000);
